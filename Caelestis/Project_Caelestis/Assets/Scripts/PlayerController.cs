8b8fee7859bc023cf316a1f881ef95daa62408bc
YifeiWANG2333 <ywang634@usc.edu>
2021-09-21 21:23:28 -0700

    Super Jump
    space + v

--- Caelestis/Project_Caelestis/Assets/Scripts/PlayerController.cs
+++ Caelestis/Project_Caelestis/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;  // declare rigid body
     private Collider2D coll; // declare box-collider
-    
+
 
     public float hSpeed; // horizontal movement speed
     public float vForce; // vertical jump force
@@ -17,7 +17,8 @@ public class PlayerController : MonoBehaviour
     public bool isGrounded; // shows 1 when player is grounded
     public bool isJumping; // shows 1 when player is jumping
 
-    bool jumpPressed; // shows 1 when JUMP button is pressed
+    bool superJump; // true when JUMP button is pressed
+    bool normalJump; // true when JUMP and V are pressed
     int jumpTimes; // times left that the player can jump
 
 
@@ -31,15 +32,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // When the JUMP button is pressed,
-        // and the times the player can jump is larger than 0,
-        // jumpPressed will be true
-        if(Input.GetButtonDown("Jump") && jumpTimes > 0)
-        {
-            jumpPressed = true;
-        }
+        JumpCheck();
 
         Attack();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, platform_test); // set up the gounding check point of the player
+
+        HorizontalMovement();
+
+        Jump();
+
     }
 
     //modified by 李道源
@@ -58,56 +63,75 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
-
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, platform_test); // set up the gounding check point of the player
-
-        HorizontalMovement();
-
-        Jump();
-
-    }
-
     void HorizontalMovement()
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal"); // get the horizontal axis value of the player: leftwards: -1 rightwards: 1 still: 0
         rb.velocity = new Vector2(horizontalMove * hSpeed, rb.velocity.y); // player moves with a steady velocity
 
-        if(horizontalMove != 0)
+        if (horizontalMove != 0)
         {
             transform.localScale = new Vector3(horizontalMove, 1, 1); // player can change its direction when moving
         }
     }
+    void JumpCheck()
+    {
+        // When the JUMP button is pressed,
+        // the times the player can jump is larger than 0 and if v is pressed either,
+        // superJump will be true
+        // if not, normal Jump will be true
+        if (Input.GetButtonDown("Jump") && jumpTimes > 0)
+        {
+            if (Input.GetKey(KeyCode.V))
+            {
+                superJump = true;
+            }
+            else
+            {
+                normalJump = true;
+            }
 
+        }
+    }
     void Jump()
     {
-        if(isGrounded)
+        if (isGrounded)
         {
             jumpTimes = 2; // when the player is grounded, resume its jumpTimes
             isJumping = false;
         }
 
-        if(jumpPressed && isGrounded)
+        if (normalJump && isGrounded)
         {
             isJumping = true;
             rb.velocity = new Vector2(rb.velocity.x, vForce); // player jumps when player is grounded and JUMP button is pressed;
             jumpTimes--;
-            jumpPressed = false;
+            normalJump = false;
         }
-        else if(jumpPressed && jumpTimes > 0 && isJumping)
+        else if (normalJump && jumpTimes > 0 && isJumping)
         {
             rb.velocity = new Vector2(rb.velocity.x, vForce); // player jumps again if JUMP button is pressed,
             jumpTimes--;                                      // and the jumpTimes is larger than 0
-            jumpPressed = false;
+            normalJump = false;
+        }
+        else if (superJump && isGrounded)
+        {
+            isJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, (1.5f * vForce)); // player jumps when player is grounded and JUMP button is pressed;
+            jumpTimes = 0;
+            superJump = false;
+        }
+        else if (superJump && jumpTimes > 0 && isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, (1.5f * vForce)); // player jumps again if JUMP button is pressed,
+            jumpTimes--;                                      // and the jumpTimes is larger than 0
+            superJump = false;
         }
     }
 
+
     void Attack()
     {
-        if (Input.GetButtonDown("Attack")) 
+        if (Input.GetButtonDown("Attack"))
         {
             anim.SetTrigger("attack"); // trigger attack animation
         }
