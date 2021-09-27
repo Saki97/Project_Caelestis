c3d90759fe7de41256986d0c6e41a926e60f7120
stao27 <shengzhuotao@gmail.com>
2021-09-27 01:40:54 -0700

    Monster Attack

--- Caelestis/Project_Caelestis/Assets/Scripts/EnemyControl.cs
+++ Caelestis/Project_Caelestis/Assets/Scripts/EnemyControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
+    private Vector3 target_position;
+    private float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
     private BoxCollider2D col;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +18,7 @@ public class EnemyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,6 +26,18 @@ public class EnemyControl : MonoBehaviour
         if(collision.gameObject.CompareTag("Player") && collision.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
         {
             Destroy(collision.gameObject);
+        }
+    }
+
+    private void Attack(){
+        // 1/2拍锁定player位置并存储
+        if(MusicHandler.Instance.CheckInputTiming() != true){
+            target_position = GameObject.FindGameObjectWithTag("Player").transform.position;
+            
+        }
+        //正拍monster冲向1/2拍时储存的位置
+        if(MusicHandler.Instance.CheckInputTiming()){
+            transform.position = Vector3.SmoothDamp(transform.position, target_position, ref velocity, smoothTime);
         }
     }
 }
