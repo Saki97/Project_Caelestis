b31af65f03a47d06c4fe10578612f66d45855467
shuo_chen <schen100@usc.edu>
2021-10-20 19:28:49 -0700

    main menu

--- Caelestis/Project_Caelestis/Assets/Scripts/MainMenu.cs
+++ Caelestis/Project_Caelestis/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Study_Scene");
     }
 
     public void QuitGame(){
